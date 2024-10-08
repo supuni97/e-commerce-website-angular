e421d354dade45813885e5a11c5a8d307e65d87a
Supuni Sithara <sitharabandara1997@gmail.com>
2024-10-08 11:20:24 +0530

    Added navbar

--- src/app/pages/admin/login/login.component.ts
+++ src/app/pages/admin/login/login.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  loginObj: any = {
+    username:'',
+    password:''
+  };
+
+  constructor(private router: Router){}
+
+  onLogin(){
+    if(this.loginObj.username == "admin" && this.loginObj.password == "1234"){
+      this.router.navigateByUrl('/products');
+      //alert('correct credentials');
+    } else {
+      alert('Wrong credentials');
+    }
+    
+  }
 
 }
